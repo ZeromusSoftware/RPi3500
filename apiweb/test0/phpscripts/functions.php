efc2af71699cc909cfebc7a364a9d7b735db8463
ZeromusSoftware <zeromus.linux@gmail.com>
2016-03-16 21:01:57 +0100

    First test : API Twitter with PHP/AJAX and OAuth. There are still issues..

---
+++ apiweb/test0/phpscripts/functions.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+require_once('TwitterAPIExchange.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+	'oauth_access_token' => "709007460401606656-cbFxI1puyVymLQjd2psTJ6QjFa9l47F",
+	'oauth_access_token_secret' => "30ugVkeKsYFuqeMKmuq2IdaOa6TGkVYSPuobWTVGTW9eY",
+	'consumer_key' => "B7WTNJIYF0FSgzzpcRH6KZon8",
+	'consumer_secret' => "gwc7F2up1QYVgTO795hfv2gDlU9wgstU44vYuZ6hnEpRJUCmlE"
+);
+
+?>
---
+++ apiweb/test1/info.php
@@ -0,0 +1,20 @@
+<?php
+/*------------------------------------------------------*/
+/*--------------------- ALL IMPORTANT DATA -------------*/
+/*------------------------------------------------------*/
+
+
+session_start();
+
+
+/*---------------- From http://apps.twitter.com-----------*/
+$consumer_key = "RfguuVtq4T9Xz3PeYBc2sCIEV";
+$consumer_secret = "Ov3dDApzrFpckTcEAMUmwvA9k7Y9SEnDHg7T0AlBEAvTOVNGuI";
+$oauth_token = "709007460401606656-cbFxI1puyVymLQjd2psTJ6QjFa9l47F";
+$oauth_token_secret = "30ugVkeKsYFuqeMKmuq2IdaOa6TGkVYSPuobWTVGTW9eY";
+
+
+/*----- From http://github.com/abraham/twitteroauth ------*/
+require_once('twitteroauth/src/TwitterOAuth.php');
+
+?>
